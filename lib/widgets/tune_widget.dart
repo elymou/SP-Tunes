02ab9ac282191e03c17fb92e82c3104abe3f83fd
elymou <mou4adelyamani@gmail.com>
2023-11-01 10:33:57 -0700

    'icons'

--- lib/widgets/tune_widget.dart
+++ lib/widgets/tune_widget.dart
@@ -3,18 +3,26 @@ import 'tune_model.dart';
 
 class Tunewidget extends StatelessWidget {
   final TuneModel tune;
-  
+
   const Tunewidget({required this.tune});
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: GestureDetector(onTap: () {
-          tune.playSound();
-        },
-          child: Container( 
-              color: tune.color,
-            ),
-        ));
+        child: GestureDetector(
+      onTap: () {
+        tune.playSound();
+      },
+      child: Container(
+        color: tune.color,
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+             Icon(Icons.music_note_sharp),
+              Icon(Icons.music_note_sharp),
+          ],
+        ),
+      ),
+    ));
   }
 }
